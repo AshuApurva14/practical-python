6b0771e33bfd9c48db8c717850cb69f029d4723d
Ashutosh Apurva <apooorva01@gmail.com>
2025-09-22 03:21:55 +0000

    add daily challenge dir

---
+++ daily_challenge/for_loop2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# print sum of all even numbers between 1 to 50
+
+sum = 0
+
+for i in range(0, 50, 2):
+    sum = sum+i 
+print(sum)
---
+++ daily_challenge/for_loop3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Print a list using for loop
+
+city = ['Mumbai', 'Delhi', 'Jaipur', 'Jodhpur', 'Lucknow', 'Indore']
+
+for i in city:
+    print(i)
