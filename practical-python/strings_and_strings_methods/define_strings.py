78bf2bcbdfd4413ee5e0d63cda235b27a1aee9db
AshuApurva14 <apooorva01@gmail.com>
2025-11-29 05:43:14 -0500

    learnt strings and methods

---
+++ practical-python/strings_and_strings_methods/define_strings.py
@@ -0,0 +1,12 @@
+# Define simple string in python
+
+fruit = "Mango"
+
+print(type(fruit))
+
+print(fruit)
+
+
+# Define length of a string
+
+print(type(len(fruit)))
---
+++ practical-python/strings_and_strings_methods/exercise1.py
@@ -0,0 +1,7 @@
+#  Print a string that uses double quotation marks inside the string.
+
+# Use single quote to define a string when string contains a double quoted substrings
+
+double_quotes_string = 'My name is "Ashutosh Apurva"'
+
+print(double_quotes_string)
