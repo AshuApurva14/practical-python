0b090670f1e75900849db350db80133c1c36f26d
Ashutosh Apurva <apooorva01@gmail.com>
2025-12-06 17:34:32 +0000

    add nested list questions

---
+++ daily_challenge/Day_7/nested_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+# Nested list example
+
+matrix = [[[0, 1, 2], [0, 1, 2], [0, 1, 2]], [[0, 1, 2], [0, 1, 2], [0, 1, 2]], [[0, 1, 2], [0, 1, 2], [0, 1, 2]]]
+
+matrix2 = []
+
+for submatrix in matrix:
+  for val in submatrix:
+    matrix2.append(val)
+
+print(matrix2[2][2])
